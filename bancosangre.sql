470637c40fc799237db904da814588b761ce0099
gerusa <142358162+gerisedlar15@users.noreply.github.com>
2026-04-06 20:31:17 -0300

    primer commit
    Created tables for Estado and Donante with initial data.

--- bancosangre.sql
+++ bancosangre.sql
@@ -1,1 +1,23 @@
+ -- Creamos la tabla de Estados
+CREATE TABLE Estado (
+    id_estado INT PRIMARY KEY,
+    descripcion VARCHAR(50)
+);
 
+-- Creamos la tabla de Donantes
+CREATE TABLE Donante (
+    dni INT PRIMARY KEY,
+    nombre VARCHAR(50),
+    apellido VARCHAR(50),
+    id_estado INT,
+    FOREIGN KEY (id_estado) REFERENCES Estado(id_estado)
+);
+
+-- Cargamos datos para que no esté vacía
+INSERT INTO Estado (id_estado, descripcion) VALUES 
+(1, 'Apto'), 
+(2, 'Pendiente');
+
+INSERT INTO Donante (dni, nombre, apellido, id_estado) VALUES 
+(12345678, 'Geraldine', 'Ingeniera', 1),
+(87654321, 'Juan', 'Perez', 2);
